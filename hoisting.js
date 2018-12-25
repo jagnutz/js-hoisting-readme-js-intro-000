3540a69892c9819f4c65c4eb07c9c6f9ef8dd7b8
Michael <chaphlux@yahoo.com>
2018-12-25 20:18:08 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -4,18 +4,31 @@ function callMe() {
   console.log("and this is crazy..");
   console.log("but here's my number..");
   console.log("so call me");
+  console.log(lyric);
   return lyric;
 }
 
 
 function crazy() {
   // fix the code in here:
-   var thisIsCrazy = function (){
+   var thisIsCrazy 
+   thisIsCrazy = function (){
     console.log("hey!!!")
   }
   thisIsCrazy();
-
 }
+
+function crazy() {
+  // fix the code in here:
+   var thisIsCrazy 
+   thisIsCrazy = function (){
+    console.log("hey!!!")
+  }
+  thisIsCrazy();
+}
+
+crazy()
+
 var name = "Cricky";
 function sayMyName() {
   // fix the code in here:
